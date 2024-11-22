508ffee598ac6176dbcba6d8c04201a9ceb18c93
user-64bit <arth.prajapati@outlook.com>
2024-11-22 12:04:19 +0530

    force to fill details

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -2,26 +2,53 @@ import getUserByEmail from "@/actions/getUserByEmail";
 import Dashboard from "@/components/dashboard";
 import { WalletAdapterWrapper } from "@/components/wallet-adapter-wrapper";
 import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 export default async function DashboardPage() {
   const session = await auth();
   const data = await getUserByEmail({
     email: session?.user?.email!,
   });
+  const {
+    profile_image,
+    cover_image,
+    username,
+    display_name,
+    description,
+    x_username,
+    github_username,
+    instagram_username,
+    linkedin_username,
+    solana_public_key,
+  } = data!;
+  if (
+    !profile_image ||
+    !cover_image ||
+    !username ||
+    !display_name ||
+    !description ||
+    !x_username ||
+    !github_username ||
+    !instagram_username ||
+    !linkedin_username ||
+    !solana_public_key
+  ) {
+    redirect("/edit-profile");
+  }
   return (
     <>
       <WalletAdapterWrapper>
         <Dashboard
-          profileImage={data?.profile_image!}
-          coverImage={data?.cover_image!}
-          username={data?.username!}
-          displayName={data?.display_name!}
-          description={data?.description!}
-          x_username={data?.x_username!}
-          github_username={data?.github_username!}
-          instagram_username={data?.instagram_username!}
-          linkedin_username={data?.linkedin_username!}
-          solana_address={data?.solana_public_key!}
+          profileImage={profile_image}
+          coverImage={cover_image}
+          username={username}
+          displayName={display_name}
+          description={description}
+          x_username={x_username}
+          github_username={github_username}
+          instagram_username={instagram_username}
+          linkedin_username={linkedin_username}
+          solana_address={solana_public_key}
         />
       </WalletAdapterWrapper>
     </>
